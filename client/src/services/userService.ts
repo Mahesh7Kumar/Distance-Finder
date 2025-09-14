f59c9df6579cb173aad69814433c230332f074a4
Mahesh <165754534+Mahesh7Kumar@users.noreply.github.com>
2025-09-14 11:38:54 +0530

    Update userService.ts

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import type { IUser } from '../types'
 
 const API = axios.create({
-    baseURL: (import.meta.env.VITE_API_BASE || "http://localhost:4000/api") + "/users"
+    baseURL: (import.meta.env.VITE_API_BASE || "https://distance-finder.onrender.com/api") + "/users"
 });
 
 export const getUsers = async() : Promise<IUser[]> =>{
